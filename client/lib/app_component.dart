44578b483b02e00472d377897de21389079af4c2
mandui tech <i-mandui@outlook.com>
2019-03-07 11:56:00 +0800

    add global style and some test

--- client/lib/app_component.dart
+++ client/lib/app_component.dart
@@ -7,7 +7,7 @@ import 'src/todo_list/todo_list_component.dart';
 
 @Component(
   selector: 'my-app',
-  styleUrls: ['app_component.css'],
+  styleUrls: ['app_component.scss.css'],
   templateUrl: 'app_component.html',
   directives: [TodoListComponent],
 )
